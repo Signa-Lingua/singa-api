fa2eaea17a9babdf06eb1eda7bb04a5d803ed66c
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2024-06-05 08:59:20 +0800

    chore: Update adonisrc.ts and initialize_bouncer_middleware.ts

--- adonisrc.ts
+++ adonisrc.ts
@@ -10,7 +10,11 @@ export default defineConfig({
   | will be scanned automatically from the "./commands" directory.
   |
   */
-  commands: [() => import('@adonisjs/core/commands'), () => import('@adonisjs/lucid/commands'), () => import('@adonisjs/bouncer/commands')],
+  commands: [
+    () => import('@adonisjs/core/commands'),
+    () => import('@adonisjs/lucid/commands'),
+    () => import('@adonisjs/bouncer/commands'),
+  ],
 
   /*
   |--------------------------------------------------------------------------
@@ -34,7 +38,7 @@ export default defineConfig({
     () => import('@adonisjs/auth/auth_provider'),
     () => import('@adonisjs/ally/ally_provider'),
     () => import('./app/providers/google_cloud_service_provider.js'),
-    () => import('@adonisjs/bouncer/bouncer_provider')
+    () => import('@adonisjs/bouncer/bouncer_provider'),
   ],
 
   /*
